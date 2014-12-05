ed14ad0e301f7be5866a728b8f99e0668136558b
avishek143 <avishekbanerjee.ranchi@gmail.com>
2014-12-06 02:22:23 +0530

    added login logic

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,24 @@
+var User = require('../lib/user');
+
 exports.form = function(req, res){
 	res.render('login', {title: 'Login'});
 };
 
 exports.submit = function(req, res, next){
+	var username = req.body.user.name;
+	var pass = req.body.user.pass;
 
+	User.authenticate(username, pass, function(err, user){
+		if(err) return next(err);
+
+		if(user){
+			req.session.uid = user.id;
+			res.redirect('/');
+		} else {
+			res.error('Invalid user credentials!');
+			res.redirect('back');
+		}
+	});
 };
 
 exports.logout = function(req, res){
