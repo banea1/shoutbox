3342955a91b13e6c19c77fe247514cbc21530493
avishek143 <avishekbanerjee.ranchi@gmail.com>
2014-12-15 18:17:28 +0530

    correctd params from param

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ var User = require('../lib/user');
 exports.auth = express.basicAuth(User.authenticate);
 
 exports.user = function(req, res, next){
-  User.get(req.param.id, function(err, user){
+  User.get(req.params.id, function(err, user){
     if(err) return next(err);
     if(!user.id) return res.send(404);
     res.json(user);
