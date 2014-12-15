4403ab4ab9ae06c4bdaaa6f5107f1fc56e32eb9f
avishek143 <avishekbanerjee.ranchi@gmail.com>
2014-12-15 18:07:32 +0530

    changed req and res arguments

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ var User = require('../lib/user');
 
 exports.auth = express.basicAuth(User.authenticate);
 
-exports.user = function(res, req, next){
+exports.user = function(req, res, next){
   User.get(req.param.id, function(err, user){
     if(err) return next(err);
     if(!user.id) return res.send(404);
